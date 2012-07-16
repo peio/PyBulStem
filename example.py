2a9d00a5542e3904c0ed43a4940eb8d505d4ed48
Peio Popov <peio@peio.org>
2012-07-16 16:44:07 +0300

    Better example

--- example.py
+++ example.py
@@ -4,12 +4,11 @@
 # PyBulStem example usage
 
 from nltk.tokenize import word_tokenize, sent_tokenize, wordpunct_tokenize
-from bulstem import stem
+from bulstem import stem, MIN_WORD_LEN
 
 text = u""" "Името на Мирослава Тодорова ми стана известно, когато към мен се обърнаха жертвите на престъпността", заяви по повод дисциплинарното уволнение на съдията вицепремиерът и министър на вътрешните работи Цветан Цветанов. В сутрешния блок на БНТ Цветанов наблегна на проблемите в съдебната система, като започна разговора с думите "ВСС е независима съдебна институция, която не бих искал да коментирам" """ 
 
-
-
 for word in wordpunct_tokenize(text):
-	print stem(word).encode('utf-8'),
+	if len(word) >= MIN_WORD_LEN:
+		print stem(word).encode('utf-8'),
 	# print word.encode('utf-8'),
